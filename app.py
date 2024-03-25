b2a2b9c34a11891d3f6f3afca9bdc026b766379c
wrx81697 <162819261+wrx81697@users.noreply.github.com>
2024-03-25 12:26:08 +0100

    zmiana w app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template
+from markupsafe import escape
+app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name='Bartek'):
+    return render_template('hello.html', name=name)
